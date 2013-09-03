29a869a2c512da8248f692bf98ad98d8dab25a6c
Ryan Herbst <rherbst@slac.stanford.edu>
2013-09-03 19:24:18 +0000

    git-svn-id: file:///afs/slac/g/reseng/svn/repos/cob_dpm/trunk/firmware/modules@3230 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- Xcv5PcieMaster/coregen/endpoint_blk_plus_v1_15/implement/implement_stand_alone_core.bat
+++
@@ -1,4 +0,0 @@
-
-rem Synthesize the Wrapper Files
-echo 'Synthesizing wrapper files with XST';
-xst -ifn source_xst.scr
--- Xcv5PcieMaster/coregen/endpoint_blk_plus_v1_15/implement/implement_stand_alone_core.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#Synthesize the Wrapper Files
-echo 'Synthesizing wrapper files with XST';
-xst -ifn source_xst.scr
