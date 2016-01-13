fe7133bad6c98a8407f93d2a4a0177a06c9fa8ec
Ryan Herbst <rherbst@slac.stanford.edu>
2016-01-13 23:08:41 +0000

    Added licensing.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/cob_dpm/trunk/firmware/modules@8753 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- ZynqPcieMaster/hdl/ZynqPcieMaster.vhd
+++ ZynqPcieMaster/hdl/ZynqPcieMaster.vhd
@@ -7,7 +7,13 @@
 -- Description:
 -- Wrapper file for Zynq PCI Express core.
 -------------------------------------------------------------------------------
--- Copyright (c) 2013 by Ryan Herbst. All rights reserved.
+-- This file is part of 'SLAC RCE PCIE Core'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'SLAC RCE PCIE Core', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
 -------------------------------------------------------------------------------
 -- Modification history:
 -- 04/02/2013: created.
