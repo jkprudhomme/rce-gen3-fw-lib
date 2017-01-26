e1fc079ab0e649e8bf7c5eceb9ab6e3db478ffb0
ruck314 <ruckman@slac.stanford.edu>
2017-01-25 17:30:19 -0800

    bug fix

--- RceG3/ruckus.tcl
+++ RceG3/ruckus.tcl
@@ -2,8 +2,7 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load local Source Code
-loadSource -dir "$::DIR_PATH/hdl/"
-loadIpCore -dir "$::DIR_PATH/coregen/processing_system7_0/"
-# loadSource -sim_only -path "$::DIR_PATH/tb/RceG3CpuSim.vhd"
-# loadSource -sim_only -path "$::DIR_PATH/tb/RceG3SimConfig.vhd"
+loadSource -dir  "$::DIR_PATH/hdl/"
+loadSource -path "$::DIR_PATH/simlink/rtl/RceG3CpuSim.vhd"
+loadIpCore -dir  "$::DIR_PATH/coregen/processing_system7_0/"
 loadSource -sim_only -path "$::DIR_PATH/tb/rceg3_tb.vhd"
