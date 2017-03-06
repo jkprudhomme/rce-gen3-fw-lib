05a127c5dc6eeb7a36a727de1992975d68cc4a2a
Ryan Herbst <rherbst@slac.stanford.edu>
2017-03-06 15:26:03 -0800

    Added pollInterval

--- python/rceg3/_rceversion.py
+++ python/rceg3/_rceversion.py
@@ -13,6 +13,7 @@
 # copied, modified, propagated, or distributed except according to the terms 
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
+import pyrogue
 import pyrogue as pr
 import collections
 
@@ -21,42 +22,43 @@ class RceVersion(pr.Device):
         super(self.__class__, self).__init__(
             description="RCE Version and BSI register.", **kwargs)
 
-    self.add(pyrogue.variable(name='fpgaVersion', description='Fpga firmware version number',
-                              offset=0x80000000, bitsize=32, bitoffset=0, base='hex', mode='RO'))
+        self.add(pyrogue.Variable(name='fpgaVersion', description='Fpga firmware version number',
+                                  offset=0x80000000, bitsize=32, bitoffset=0, base='hex', mode='RO'))
 
-    self.add(pyrogue.variable(name='scratchPad', description='Scratchpad Register',
-                              offset=0x80000004, bitsize=32, bitoffset=0, base='hex', mode='RW'))
+        self.add(pyrogue.Variable(name='scratchPad', description='Scratchpad Register',
+                                  offset=0x80000004, bitsize=32, bitoffset=0, base='hex', mode='RW'))
 
-    self.add(pyrogue.variable(name='rceVersion', description='RCE registers version number',
-                              offset=0x80000008, bitsize=32, bitoffset=0, base='hex', mode='RO'))
+        self.add(pyrogue.Variable(name='rceVersion', description='RCE registers version number',
+                                  offset=0x80000008, bitsize=32, bitoffset=0, base='hex', mode='RO'))
 
-    self.add(pyrogue.variable(name='deviceDna', description='Xilinx Device DNA Value',
-                              offset=0x80000020, bitsize=64, bitoffset=0, base='hex', mode='RO'))
+        self.add(pyrogue.Variable(name='deviceDna', description='Xilinx Device DNA Value',
+                                  offset=0x80000020, bitsize=64, bitoffset=0, base='hex', mode='RO'))
 
-    self.add(pyrogue.variable(name='eFuseValue', description='Xilinx E-Fuse Value',
-                              offset=0x80000030, bitsize=32, bitoffset=0, base='hex', mode='RO'))
+        self.add(pyrogue.Variable(name='eFuseValue', description='Xilinx E-Fuse Value',
+                                  offset=0x80000030, bitsize=32, bitoffset=0, base='hex', mode='RO'))
 
-    self.add(pyrogue.variable(name='ethMode', description='Ethernet Mode',
-                              offset=0x80000034, bitsize=32, bitoffset=0, base='hex', mode='RO'))
+        self.add(pyrogue.Variable(name='ethMode', description='Ethernet Mode',
+                                  offset=0x80000034, bitsize=32, bitoffset=0, base='hex', mode='RO'))
 
-    self.add(pyrogue.variable(name='heartBeat', description='A constantly incrementing value',
-                              offset=0x80000038, bitsize=32, bitoffset=0, base='hex', mode='RO'))
+        self.add(pyrogue.Variable(name='heartBeat', description='A constantly incrementing value',
+                                  offset=0x80000038, bitsize=32, bitoffset=0, 
+                                  pollInterval=1, base='hex', mode='RO'))
 
-    dev.add(pyrogue.Variable(name='gitHash', description='GIT SHA-1 Hash',
-                             offset=0x80000040, bitSize=160, bitOffset=0, base='hex', mode='RO'))
+        self.add(pyrogue.Variable(name='gitHash', description='GIT SHA-1 Hash',
+                                 offset=0x80000040, bitSize=160, bitOffset=0, base='hex', mode='RO'))
 
-    dev.add(pyrogue.Variable(name='buildStamp', description='Firmware build string',
-                             offset=0x80001000, bitSize=256*8, bitOffset=0, base='string', mode='RO'))
+        self.add(pyrogue.Variable(name='buildStamp', description='Firmware build string',
+                                 offset=0x80001000, bitSize=256*8, bitOffset=0, base='string', mode='RO'))
 
-    dev.add(pyrogue.Variable(name='serialNumber', description='Serial Number',
-                             offset=0x84000140, bitSize=64, bitOffset=0, base='hex', mode='RO'))
+        self.add(pyrogue.Variable(name='serialNumber', description='Serial Number',
+                                 offset=0x84000140, bitSize=64, bitOffset=0, base='hex', mode='RO'))
 
-#    dev.add(pyrogue.Variable(name='atcaSlot', description='ATCA Slot',
-#                             offset=0x84000148, bitSize=8, bitOffset=16, base='hex', mode='RO'))
+        self.add(pyrogue.Variable(name='atcaSlot', description='ATCA Slot',
+                                  offset=0x84000148, bitSize=8, bitOffset=16, base='hex', mode='RO'))
 
-#    dev.add(pyrogue.Variable(name='cobBay', description='COB Bay',
-#                             offset=0x84000148, bitSize=8, bitOffset=8, base='hex', mode='RO'))
+        self.add(pyrogue.Variable(name='cobBay', description='COB Bay',
+                                  offset=0x84000148, bitSize=8, bitOffset=8, base='hex', mode='RO'))
 
-#    dev.add(pyrogue.Variable(name='cobElement', description='COB Element',
-#                             offset=0x84000148, bitSize=8, bitOffset=0, base='hex', mode='RO'))
+        self.add(pyrogue.Variable(name='cobElement', description='COB Element',
+                                  offset=0x84000148, bitSize=8, bitOffset=0, base='hex', mode='RO'))
 
